8b85d05d2ebca6c04098ecec4a66bc39b3224b4c
lpetrus32 <56268582+lpetrus32@users.noreply.github.com>
2020-07-30 19:18:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,20 +52,47 @@ function next(num){
             but1.style.height="29.5%";
             but2.style.height="29.5%";
             but1.textContent = "Parcours classique, Bac et licence de Bio";
-            but2.textContent = "Parcours atypique (DUT, BTS, Licence pro...)"
+            but2.textContent = "Parcours atypique (DUT, BTS, Licence pro...)";
             but1.style.visibility="visible";
             but2.style.visibility="visible";
             but3.style.visibility="visible";
             but4.style.visibility="visible";
             document.getElementById("form").style.visibility="hidden";
         }
-        console.log(answers);
     }else if(page == 3){
         answers.push(num);
-        quest.textContent="";
+        quest.textContent="Attention mise en situation ! Tu as cours à seulement 15h demain, que fais tu de ta soirée ?";
+        but1.textContent = `On rentabilise, GO Tinder !`;
+        but2.textContent = "Netflix & Chill";
+        but3.textContent = "Apéro tranquille sur les quais sans rentrer trop tard";
+        but4.textContent = "Ok alors pré-soirée chez moi, puis on enchaine les bars et on fini en boite !";
     }else if(page == 4){
         answers.push(num);
-        quest.textContent="";
+        quest.textContent = "Et sinon l'informatique ca te parle un peu ?";
+        but1.style.height = "62%";
+        but2.style.height = "62%";
+        but1.textContent = "J'en ai déja fait dans mes études ou tout seul";
+        but2.textContent = "Alors non pas vraiment c'est surtout la Bio qui m'a attiré";
+        but3.style.visibility="hidden";
+        but4.style.visibility="hidden";
+    }else if(page == 5){
+        answers.push(num);
+        quest.textContent = "Oulaaaa grosse question là, tu dis plutôt...";
+        but1.textContent = "Pain au chocolat";
+        but2.textContent = "Chocolatine";
+    }else if(page == 6){
+        answers.push(num);
+        quest.textContent = "Il est où ton ter-ter, ta terre de coeur, celle dont tu rêves la nuit ? ";
+        but1.style.height = "29.5%";
+        but2.style.height = "29.5%";
+        but1.textContent = "Nord-Est";
+        but2.textContent = "Nord-Ouest";
+        but3.style.visibility="visible";
+        but4.style.visibility="visible";
+        but3.textContent = "Sud-Est";
+        but4.textContent = "Sud-Ouest";
+        document.getElementById("B5").style.visibility="visible";
+        document.getElementById("B6").style.visibility="visible";
     }
     page++;
 }
